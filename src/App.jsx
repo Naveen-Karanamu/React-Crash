870cedfe00f49f235dc4ac087cb92c91c0199e9b
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-09-23 18:52:24 +0530

    Setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { Route } from "react-router-dom";
+import About from "./Pages/About";
+import Homepage from "./Pages/Homepage";
 
 
 function App() {
   return (
-      <></>
+      <>
+      <Route path="/home" exact component={Homepage}/>
+      <Route path="/about" exact component={About}/>
+
+      
+      </>
+
   );
 }
 
---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Homepage(){
+    return(
+        <>
+        <div>Hello from home</div>
+        </>
+    )
+}
+
+export default Homepage;
